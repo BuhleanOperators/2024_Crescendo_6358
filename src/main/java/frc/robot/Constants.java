048ee67bdc4d2b621f38ab22904919baa79b7e17
Max Young <maxyoung214@gmail.com>
2024-01-16 09:01:50 -0700

    Add all constants for drive; values are not set

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,37 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class DriveConstants {
+    //TODO Set all values
+    //!Odomotry Values
+    public static final double trackWidth = 0; //meters
+    public static final double wheelRadius = 0; //meters
+    public static final double maxSpeed = 0; //meters per second
+    public static final double maxAngularSpeed = 2 * Math.PI; //one rotation per second
+
+    public static final int rightLeadID = 0;
+    public static final int rightFollowID = 0;
+    public static final int leftLeadID = 0;
+    public static final int leftFollowID = 0;
+
+    public static final int rightDriveEncoderPort1 = 0;
+    public static final int rightDriveEncoderPort2 = 0;
+    public static final int leftDriveEncoderPort1 = 0;
+    public static final int leftDriveEncoderPort2 = 0;
+    //& Encoder Values
+    //TODO Figure out how to use NEO's hall effect sensors
+    public static final double encoderResolution = 0;
+    public static final double DistancePerPulse = 2 * Math.PI * wheelRadius / encoderResolution;
+    //* PID Controller Values
+    public static final double lP = 0;
+    public static final double lI = 0;
+    public static final double lD = 0;
+    public static final double rP = 0;
+    public static final double rI = 0;
+    public static final double rD = 0;
+    //^ FeedForward Values
+    //~ kS = 0.5 on tile and 0.95 on carpet
+    public static final double kS = 0;
+    public static final double kV = 0;
+  }
 }
