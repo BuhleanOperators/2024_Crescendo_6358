b633d6782f636634941f05a79eab713024df8d61
Max Young <maxyoung214@gmail.com>
2024-01-16 14:47:18 -0700

    IDEK

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -19,34 +21,34 @@ public final class Constants {
   public static class DriveConstants {
     //TODO Set all values
     //!Odomotry Values
-    public static final double trackWidth = 0; //meters
-    public static final double wheelRadius = 0; //meters
-    public static final double maxSpeed = 0; //meters per second
+    public static final double trackWidth = Units.inchesToMeters(23.875); //meters
+    public static final double wheelRadius = Units.inchesToMeters(3); //meters
+    public static final double maxSpeed = 3; //meters per second
     public static final double maxAngularSpeed = 2 * Math.PI; //one rotation per second
 
-    public static final int rightLeadID = 0;
-    public static final int rightFollowID = 0;
-    public static final int leftLeadID = 0;
-    public static final int leftFollowID = 0;
-
-    public static final int rightDriveEncoderPort1 = 0;
-    public static final int rightDriveEncoderPort2 = 0;
-    public static final int leftDriveEncoderPort1 = 0;
-    public static final int leftDriveEncoderPort2 = 0;
+    public static final int rightLeadID = 7;
+    public static final int rightFollowID = 4;
+    public static final int leftLeadID = 3;
+    public static final int leftFollowID = 9;
     //& Encoder Values
-    //TODO Figure out how to use NEO's hall effect sensors
-    public static final double encoderResolution = 0;
+    public static final int rightDriveEncoderPort1 = 2;
+    public static final int rightDriveEncoderPort2 = 3;
+    public static final int leftDriveEncoderPort1 = 1;
+    public static final int leftDriveEncoderPort2 = 2;
+
+    public static final double encoderResolution = 2048;
     public static final double DistancePerPulse = 2 * Math.PI * wheelRadius / encoderResolution;
     //* PID Controller Values
-    public static final double lP = 0;
+    public static final double lP = 1;
     public static final double lI = 0;
     public static final double lD = 0;
-    public static final double rP = 0;
+
+    public static final double rP = 1;
     public static final double rI = 0;
     public static final double rD = 0;
     //^ FeedForward Values
     //~ kS = 0.5 on tile and 0.95 on carpet
-    public static final double kS = 0;
-    public static final double kV = 0;
+    public static final double kS = 0.95;
+    public static final double kV = 3;
   }
 }
