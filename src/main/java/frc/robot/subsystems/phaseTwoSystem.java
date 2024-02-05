b6deb815dc0f472e51671e4b7db031951b5c1c9c
Max Young <maxyoung214@gmail.com>
2024-02-05 10:28:36 -0700

    Change to VictorSP

--- src/main/java/frc/robot/subsystems/phaseTwoSystem.java
+++ src/main/java/frc/robot/subsystems/phaseTwoSystem.java
@@ -5,14 +5,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PhaseTwoConstants;
 
 public class phaseTwoSystem extends SubsystemBase {
   /** Creates a new phaseTwoSystem. */
-  private Spark motor;
+  private VictorSP motor;
   public phaseTwoSystem() {
-    motor = new Spark(PhaseTwoConstants.motorID);
+    motor = new VictorSP(PhaseTwoConstants.motorID);
   }
 
   @Override
